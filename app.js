bf3cd45a26b0235b39c7eb61f52f72cd2208832f
Jorge Viera <Jorge5psgt@gmail.com>
2021-06-16 02:59:43 -0400

    completed work

---
+++ app.js
@@ -0,0 +1,156 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const teamMembers = [];
+const idArray = [];
+
+function appMenu() {
+
+  function createManager() {
+    console.log("Please build your team");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "Whats the Managers name?"
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "Whats the Manager's Id?"
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "Whats the Manager's email?"
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "Whats the Manager's office number?"
+      }
+      
+    ]).then(answers => {
+      const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+      teamMembers.push(manager);
+      idArray.push(answers.managerId);
+      createTeam();
+    });
+  }
+
+  function createTeam() {
+
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "memberChoice",
+        message: "Which type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members"
+        ]
+      }
+    ]).then(userChoice => {
+      switch(userChoice.memberChoice) {
+      case "Engineer":
+        addEngineer();
+        break;
+      case "Intern":
+        addIntern();
+        break;
+      default:
+        buildTeam();
+      }
+    });
+  }
+
+  function addEngineer() {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "Whats the Engineer's name?"
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "Whats the Engineer's Id?"
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "Whats the Engineer's email?"
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "Whats the Engineer's Github?"
+      } 
+
+
+    ]).then(answers => {
+      const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+      teamMembers.push(engineer);
+      idArray.push(answers.engineerId);
+      createTeam();
+    });
+  }
+     
+
+  function addIntern() {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "Whats the Intern's name?"
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "Whats the Intern's Id?"
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "Whats the Intern's email?"
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "Whats the Intern's school?"
+      }
+
+    ]).then(answers => {
+      const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+      teamMembers.push(intern);
+      idArray.push(answers.internId);
+      createTeam();
+    });
+  }
+
+   
+
+  function buildTeam() {
+    // Create the output directory if the output path doesn't exist
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
+
+  createManager();
+
+}
+
+
+appMenu();
